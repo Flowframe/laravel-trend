f43540a2976761bf39ee4da1c39f06a160c9ddd4
abangijal <ijalnasution107@gmail.com>
2022-09-29 13:52:18 +0700

    Update MySqlAdapter.php
    Implement isTimestamp

--- src/Adapters/MySqlAdapter.php
+++ src/Adapters/MySqlAdapter.php
@@ -6,7 +6,7 @@ use Error;
 
 class MySqlAdapter extends AbstractAdapter
 {
-    public function format(string $column, string $interval): string
+    public function format(string $column, string $interval, bool $isTimestamp): string
     {
         $format = match ($interval) {
             'minute' => '%Y-%m-%d %H:%i:00',
@@ -16,6 +16,10 @@ class MySqlAdapter extends AbstractAdapter
             'year' => '%Y',
             default => throw new Error('Invalid interval.'),
         };
+
+        if ($isTimestamp) {
+            return "date_format(from_unixtime({$column}), '{$format}')";
+        }
 
         return "date_format({$column}, '{$format}')";
     }
