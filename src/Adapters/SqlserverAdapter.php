f5c06d4b0aec37df9996a0afb324a39753b717f5
Zvonimir Lokmer <lokmer@gmail.com>
2024-03-19 21:28:33 +0100

    date column name enclosed with brackets to support reserved word as column name

--- src/Adapters/SqlserverAdapter.php
+++ src/Adapters/SqlserverAdapter.php
@@ -9,11 +9,11 @@ class SqlserverAdapter extends AbstractAdapter
     public function format(string $column, string $interval): string
     {
         return match ($interval) {
-            'minute' => "LEFT(CONVERT(VARCHAR, $column, 120), 17) + '00'",
-            'hour' => "LEFT(CONVERT(VARCHAR, $column, 120), 14) + '00:00'",
-            'day' => "CONVERT(VARCHAR, $column, 23)",
-            'month' => "LEFT(CONVERT(VARCHAR, $column, 23), 7)",
-            'year' => "CONVERT(VARCHAR(4), YEAR($column))",
+            'minute' => "LEFT(CONVERT(VARCHAR, [$column], 120), 17) + '00'",
+            'hour' => "LEFT(CONVERT(VARCHAR, [$column], 120), 14) + '00:00'",
+            'day' => "CONVERT(VARCHAR, [$column], 23)",
+            'month' => "LEFT(CONVERT(VARCHAR, [$column], 23), 7)",
+            'year' => "CONVERT(VARCHAR(4), YEAR([$column]))",
             default => throw new Error('Invalid interval.'),
         };
     }
